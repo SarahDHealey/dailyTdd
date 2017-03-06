7703f09af0714c30dbd34e2e0f3dac795a08f668
Sarah Healey <s.deane.healey@gmail.com>
2017-03-05 21:09:27 -0700

    tests passing for first test case

--- test/day_1.test.js
+++ test/day_1.test.js
@@ -1,8 +1,11 @@
 'use strict';
 var assert = require('assert');
 var myFuncs = require('../lib/day_1');
-describe('Checks if this stuff works', () => {
-  it('returns "YUP"', () => {
-    assert.equal(myFuncs.testFunc(), "YUP!")
+
+describe('blocksFromStart', () => {
+  it('determines the number of blocks Santa would walk', () => {
+    var input = "R2, L3"
+    var expected = 5
+    assert.equal(myFuncs.getBlocks(input), expected)
   })
 })
