090140acbe07b8f2266b4bd44cde4caa99bd1520
Sarah Healey <s.deane.healey@gmail.com>
2017-03-06 18:31:27 -0700

    input R2,R2,R2 passing tests

--- test/day_1.test.js
+++ test/day_1.test.js
@@ -3,9 +3,16 @@ var assert = require('assert');
 var myFuncs = require('../lib/day_1');
 
 describe('blocksFromStart', () => {
-  it('determines the number of blocks Santa would walk', () => {
-    var input = "R2, L3"
-    var expected = 5
-    assert.equal(myFuncs.getBlocks(input), expected)
+  describe('#getBlocks', () => {
+    it('determines the number of blocks Santa would walk if given R2, L3', () => {
+      var input = "R2, L3"
+      var expected = 5
+      assert.equal(myFuncs.getBlocks(input), expected)
+    })
+    it('determines the number of blocks Santa would walk if given R2, R2, R2', () => {
+      var input = "R2, R2, R2"
+      var expected = 2
+      assert.equal(myFuncs.getBlocks(input), expected)
+    })
   })
 })
